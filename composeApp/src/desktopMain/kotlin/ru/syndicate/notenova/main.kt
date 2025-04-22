552817323f02db40720be0fc04e41727c944b74a
zheniaregbl <evgenieregbl@gmail.com>
2025-04-22 14:52:40 +0300

    init commit

---
+++ composeApp/src/desktopMain/kotlin/ru/syndicate/notenova/main.kt
@@ -0,0 +1,13 @@
+package ru.syndicate.notenova
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "note-nova",
+    ) {
+        App()
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/ru/syndicate/notenova/MainViewController.kt
@@ -0,0 +1,5 @@
+package ru.syndicate.notenova
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
